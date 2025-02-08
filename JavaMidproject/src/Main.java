17876336afd57c9350cdc90b3fedc9fbe7974209
khayri <khayriramzy@gmail.com>
2025-02-08 17:25:53 +0700

    Tambahkan pseudocode tambahan untuk menu 1

--- JavaMidproject/src/Main.java
+++ JavaMidproject/src/Main.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-
+	Scanner scan = new Scanner(System.in);
 	public Main() {
 		ArrayList<Karyawan> data = new ArrayList<Karyawan>();
 		while(true) {
 			// minta user untuk memilih dari 4 menu (insert data karyawan, view data karyawan, 
 			// update data karyawan, dan delete data karyawan)
 			
+			
 			// jika user memilih 1 (insert data karyawan)
 				// minta input data untuk karyawan
+				// kode karyawan terdiri dari MM-XXXX dimana MM adalah 2 huruf afabet yang di random 
+				// dilanjutkan dengan 4 angka numeric yang di random (contoh : ZX-3145),
+				// nama karyawan
+				// jenis kelamin (Laki-Laki / Perempuan)
+				// jabatan (Manager / Supervisor / Admin)
+			
 				// lalu masukkan data ke dalam arraylist karyawan
 			
 			// jika user memilih 2 (view data karyawan)
