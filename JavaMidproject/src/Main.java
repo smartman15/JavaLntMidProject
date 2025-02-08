d17e4cbf5e367c9634975beb3a9f43f7162a6a85
khayri <khayriramzy@gmail.com>
2025-02-08 17:48:02 +0700

    Tuliskan kode untuk pseudocode

--- JavaMidproject/src/Main.java
+++ JavaMidproject/src/Main.java
@@ -8,17 +8,63 @@ public class Main {
 		while(true) {
 			// minta user untuk memilih dari 4 menu (insert data karyawan, view data karyawan, 
 			// update data karyawan, dan delete data karyawan)
+			System.out.println("choose something bruh");
+			int menu = scan.nextInt(); scan.nextLine();
+			
+			
 			
 			
 			// jika user memilih 1 (insert data karyawan)
+			if(menu == 1) {
 				// minta input data untuk karyawan
 				// kode karyawan terdiri dari MM-XXXX dimana MM adalah 2 huruf afabet yang di random 
 				// dilanjutkan dengan 4 angka numeric yang di random (contoh : ZX-3145),
+				String kode;
+				System.out.println("Masukkan kode:");
+				kode = scan.nextLine(); 
 				// nama karyawan
+				String nama;
+				System.out.println("Masukkan nama karyawan:");
+				nama = scan.nextLine(); 
 				// jenis kelamin (Laki-Laki / Perempuan)
+				String kelamin;
+				System.out.println("Masukkan jenis kelamin (Laki-Laki / Perempuan):");
+				kelamin = scan.nextLine(); 
 				// jabatan (Manager / Supervisor / Admin)
-			
+				String jabatan;
+				System.out.println("Masukkan jabatan (Manager / Supervisor / Admin):");
+				jabatan = scan.nextLine(); 
+//				Untuk gaji karyawan terdiri dari:
+//					● Manager = Rp. 8 Juta.
+//					● Supervisor = Rp. 6 Juta.
+//					● Admin = Rp. 4 Juta.
+				int gaji = 0;
+				if(jabatan == "Manager") {
+					gaji = 8000000;
+				} else if(jabatan == "Supervisor") {
+					gaji = 6000000;
+				} else if(jabatan == "Admin") {
+					gaji = 4000000;
+				} 
+				
+				
+				System.out.println("data finished");
 				// lalu masukkan data ke dalam arraylist karyawan
+				Karyawan k1 = new Karyawan(kode, nama, kelamin, jabatan, gaji);
+				data.add(k1);
+				
+				for(int i = 0; i < data.size(); i++) {
+					System.out.println(data.get(i).kode);
+					System.out.println(data.get(i).nama);
+					System.out.println(data.get(i).kelamin);
+					System.out.println(data.get(i).jabatan);
+					System.out.println(data.get(i).gaji);
+				}
+				
+				break;
+			}
+				
+			
 			
 			// jika user memilih 2 (view data karyawan)
 				// gunakan for loop untuk print data semua karyawan dari arraylist
