823eca6cb9a38d7ddf3de692fbec4a1e836cbd95
khayri <khayriramzy@gmail.com>
2025-02-08 15:21:10 +0700

    Bikin properti untuk class Karyawan

--- JavaMidproject/src/Karyawan.java
+++ JavaMidproject/src/Karyawan.java
@@ -1,4 +1,16 @@
 
 public class Karyawan {
-
+	String kode;
+	String nama;
+	String kelamin
+	String jabatan;
+	
+	public Karyawan(String kode, String nama, String kelamin, String jabatan) {
+		super();
+		this.kode = kode;
+		this.nama = nama;
+		this.kelamin = kelamin;
+		this.jabatan = jabatan;
+	}
+	
 }
