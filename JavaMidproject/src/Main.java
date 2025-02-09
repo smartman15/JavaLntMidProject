9c522e5285f2d47710086b56c6c84964779e9f07
khayri <khayriramzy@gmail.com>
2025-02-09 13:37:48 +0700

    Tambahkan kode untuk menu 3 (update data karyawan)

--- JavaMidproject/src/Main.java
+++ JavaMidproject/src/Main.java
@@ -75,9 +75,59 @@ public class Main {
 			
 			
 			// jika user memilih 3 (update data karyawan)
+			else if(menu == 3) {
 				// gunakan for loop untuk print data semua karyawan (dengan nomor urutan juga)
+				System.out.printf("%-25s | %-25s | %-25s | %-25s | %-25s | %-25s\n", "No", "Kode Karyawan", "Nama Karyawan", "Jenis Kelamin", "Jabatan", "Gaji Karyawan");
+				for(int i = 0; i < data.size(); i++) {
+					System.out.printf("%-25s | %-25s | %-25s | %-25s | %-25s | %-25d\n", i+1, data.get(i).kode, data.get(i).nama, data.get(i).kelamin, data.get(i).jabatan, data.get(i).gaji);
+					
+				}
 				// minta user untuk pilih nomor urutan karyawan yang ingin diupdate
+				System.out.println("Input nomor urutan karyawan yang ingin diupdate: ");
+				int nomor = scan.nextInt(); scan.nextLine();
 				// masukkan data karyawan
+				int length = data.size();
+				for(int i = 0; i < length; i++) {
+					if((i+1) == nomor) {
+						
+						// nama karyawan
+						String nama;
+						System.out.println("Masukkan nama karyawan:");
+						nama = scan.nextLine(); 
+						data.get(i).setNama(nama);
+						
+						// jenis kelamin (Laki-Laki / Perempuan)
+						String kelamin;
+						System.out.println("Masukkan jenis kelamin (Laki-Laki / Perempuan) (Case Sensitive):");
+						kelamin = scan.nextLine(); 
+						data.get(i).setKelamin(kelamin);
+						
+						// jabatan (Manager / Supervisor / Admin)
+						String jabatan;
+						System.out.println("Masukkan jabatan (Manager / Supervisor / Admin) (Case Sensitive):");
+						jabatan = scan.nextLine(); 
+						data.get(i).setJabatan(jabatan);
+						
+//						Untuk gaji karyawan terdiri dari:
+//							● Manager = Rp. 8 Juta.
+//							● Supervisor = Rp. 6 Juta.
+//							● Admin = Rp. 4 Juta.
+						int gaji = 0;
+						if(jabatan.equals("Manager")) {
+							gaji = 8000000;
+						} else if(jabatan.equals("Supervisor")) {
+							gaji = 6000000;
+						} else if(jabatan.equals("Admin")) {
+							gaji = 4000000;
+						} 
+						data.get(i).setGaji(gaji);
+						
+						
+						System.out.println("Berhasil mengupdate karyawan dengan id " + data.get(i).kode);
+					}
+				}
+			}
+				
 			
 			// jika user memilih 4 (delete data karyawan)
 				// gunakan for loop untuk print data semua karyawan
